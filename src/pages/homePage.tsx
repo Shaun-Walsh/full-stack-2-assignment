4957b781c6e0a23be4e3f0ee7f0de0240c3fecfb
Shaun Walsh <shaunwalsh24@gmail.com>
2025-08-29 15:38:40 +0100

    implemented basic pagination on discover movies homepage

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PageTemplate from "../components/templateMovieListPage";
 import { getMovies } from "../api/tmdb-api";
 import useFiltering from "../hooks/useFiltering";
@@ -10,6 +10,7 @@ import { DiscoverMovies, BaseMovieProps } from "../types/interfaces";
 import { useQuery } from "react-query";
 import Spinner from "../components/spinner";
 import AddToFavouritesIcon from "../components/cardIcons/addToFavourites";
+import { Pagination, Box } from "@mui/material";
 
 const titleFiltering = {
   name: "title",
@@ -22,10 +23,18 @@ const genreFiltering = {
   condition: genreFilter,
 };
 
+// Pagination is used to navigate through the pages of the movies by using the page state to send the page number to the getMovies function
+// keepPreviousData shows previous page data while loading new page to prevent UI flickering/loading states
+// Sourced from https://tanstack.com/query/v3/docs/framework/react/guides/paginated-queries link in tutors leads to a slightly different implementation of pagination
+
 const HomePage: React.FC = () => {
-  const { data, error, isLoading, isError } = useQuery<DiscoverMovies, Error>(
-    "discover",
-    getMovies
+  const [page, setPage] = useState(1);
+  const { data, error, isLoading, isError, isFetching } = useQuery<DiscoverMovies, Error>(
+    ["discover", page],
+    () => getMovies(page),
+    {
+      keepPreviousData: true,
+    }
   );
   const { filterValues, setFilterValues, filterFunction } = useFiltering([
     titleFiltering,
@@ -52,6 +61,8 @@ const HomePage: React.FC = () => {
   const movies = data ? data.results : [];
   const displayedMovies = filterFunction(movies);
 
+// MUI controlled pagination component navigates through movie pages using controlled page state. Pagination is disabled when fetching new data to prevent UI flickering/loading states
+// Follows controlled pagination pattern from: https://mui.com/material-ui/react-pagination/#controlled-pagination
   return (
     <>
       <PageTemplate
@@ -66,6 +77,17 @@ const HomePage: React.FC = () => {
         titleFilter={filterValues[0].value}
         genreFilter={filterValues[1].value}
       />
+      <Box display="flex" justifyContent="center" sx={{ mt: 3, mb: 2 }}>
+        <Pagination 
+          count={data?.total_pages || 1} 
+          page={page} 
+          onChange={(_, value) => setPage(value)}
+          disabled={isFetching}
+          color="primary"
+          size="large"
+        />
+      </Box>
+      {isFetching && <Box textAlign="center" sx={{ mt: 1 }}>Loading...</Box>}
     </>
   );
 };
